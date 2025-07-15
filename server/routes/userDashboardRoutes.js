00ce8e9c0cd632e62f413d4d0d774ef053cfc138
UGONES <ugochukwuhumphreyezeamaku@gmail.com>
2025-07-15 20:40:19 +0100

    corrected some spelling

--- server/routes/userDashboardRoutes.js
+++ server/routes/userDashboardRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { verifyToken, isUser } from "../middleware/authMiddleware.js";
-import { getUserSummary } from "../controllers/userdController.js";
 import { getOwnScrapes } from "../controllers/scrapeController.js";
-import { getUserProfile, updateUserProfile } from "../controllers/userController.js";
+import {
+    getUserProfile,
+    updateUserProfile,
+    getUserSummary
+} from "../controllers/userController.js";
 
 const router = express.Router();
 router.use(verifyToken, isUser);
